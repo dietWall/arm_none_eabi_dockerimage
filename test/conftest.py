0c706deabaeaea4618bde6c516fb47d27b9b776a
Dietrich Wall <dietrich7@gmx.de>
2026-02-02 19:00:51 +0100

    cleanup of conftest added checks for tools availability
    Signed-off-by: Dietrich Wall <dietrich7@gmx.de>

--- test/conftest.py
+++ test/conftest.py
@@ -1,10 +1,32 @@
 import pytest
+import docker
 
+default_tag = "stm32"
 
 def pytest_addoption(parser):
-    parser.addoption("--container_id",action="store",default="default_value",help="defines a container id to use",)
+    parser.addoption("--tag",action="store",help="defines a container id to use", default=default_tag)
 
+def get_repo_root() -> str:
+    import subprocess    
+    git_result = subprocess.run(["git", "rev-parse", "--show-toplevel"],capture_output=True)
+    repo_root = git_result.stdout.strip().decode("utf-8")
+    return repo_root
 
-@pytest.fixture
-def container_id(request):
-    return request.config.getoption("--container_id")
+def run_container(img):
+    from docker.types import Mount
+    client = docker.from_env()
+    repo_mount = Mount(target=f"/home/developer/", source=get_repo_root(), type='bind')
+    result = client.containers.run(image=default_tag, 
+                                   detach=True, stdout=True, stdin_open=True, 
+                                   mounts=[repo_mount],
+                                   remove=True)
+    print(f"Started container with id: {result.id}")
+    return result
+
+@pytest.fixture(scope="module")
+def container(request):
+    img = request.config.getoption("--tag")
+    container = run_container(img=img)
+    yield container
+
+    container.stop()
--- test/test_compile.py
+++ test/test_compile.py
@@ -1,12 +1,21 @@
 import pytest
 
-import docker
 
-def test_compile(container_id, source_path="/home/developer/code/test/lib"):
+@pytest.mark.parametrize("tool", ["gdb-multiarch", "cmake", "arm-none-eabi-gcc"])
+def test_tools_available(container, tool):
     print("")   #newline for readability
-    print(f"testing in container {container_id}")
-    client = docker.from_env()
-    container = client.containers.get(container_id)
+    
+    gdb_output = container.exec_run(cmd=f"{tool} --version")
+    for l in gdb_output.output.decode("utf-8").splitlines():
+        print(l)
+    assert gdb_output.exit_code == 0, f"gdb exit code: {gdb_output.exit_code}, output: {gdb_output.output.decode("utf-8").splitlines()}"
+
+
+def test_compile(container, 
+                 source_path="/home/developer/test/lib/"):
+    print("")   #newline for readability
+    print(f"testing in container {container.id}")
+    
     #make sure build directory is clean
     container.exec_run(cmd="rm -rf build/*")
     #make sure build directory exists
@@ -19,3 +28,6 @@ def test_compile(container_id, source_path="/home/developer/code/test/lib"):
     assert make_result.exit_code == 0, f"make failed with output: {make_result.output.decode('utf-8')}"
     print("make succeeded")
     print("Test passed")
+
+
+
