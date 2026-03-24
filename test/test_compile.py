dcce187dd53267e71e5a90ad062eb2ecd53483e2
Dietrich Wall <dietrich7@gmx.de>
2026-03-24 13:55:23 +0100

    cleanup

--- test/test_compile.py
+++ test/test_compile.py
@@ -7,14 +7,11 @@ def test_compile(container_id, source_path="/home/developer/code/test/lib"):
     print(f"testing in container {container_id}, source path: {source_path}")
     
     client = docker.from_env()
-    container = client.containers.get(container_id)
-    dir_contents = container.exec_run(cmd="ls -la", workdir=source_path)
-    print(f"Contents of {source_path} in container:")
-    print(dir_contents.output.decode('utf-8'))
-    
+    container = client.containers.get(container_id)   
     copied_source_path = f"/home/developer/workspace"
     build_dir = "/home/developer/build"
-    mkdir_result = container.exec_run(cmd=f"mkdir -p {copied_source_path}")
+    
+    container.exec_run(cmd=f"mkdir -p {copied_source_path}")
     
     dir_contents = container.exec_run(cmd="ls -la", workdir=copied_source_path)
     print(f"Contents of {copied_source_path} in container:")
@@ -22,14 +19,6 @@ def test_compile(container_id, source_path="/home/developer/code/test/lib"):
 
     # github actions run with a different uid, we make a copy with changed ownership
     cp_result = container.exec_run(cmd=f"cp -r {source_path} {copied_source_path}", workdir=copied_source_path)
-    print(f"cp: {cp_result.exit_code}")
-    print(cp_result.output.decode('utf-8'))
-
-
-    dir_contents = container.exec_run(cmd="ls -la", workdir=copied_source_path)
-    print(f"Contents of {copied_source_path} in container:")
-    print(dir_contents.output.decode('utf-8'))
-
     container.exec_run(cmd=f"chown -R developer:developer {copied_source_path}", workdir=copied_source_path)
 
     #make sure build directory is clean
