f9f1fe75d1f436ecd9ec1b34a77af0bd90e7b2ab
Dietrich Wall <dietrich7@gmx.de>
2026-03-24 09:21:39 +0100

    fix test

--- test/test_compile.py
+++ test/test_compile.py
@@ -8,14 +8,14 @@ def test_compile(container_id, source_path="/home/developer/code/test/lib"):
     client = docker.from_env()
     container = client.containers.get(container_id)
     #make sure build directory is clean
-    container.exec_run(cmd="rm -rf build/*")
+    container.exec_run(cmd="rm -rf build/*", workdir=source_path)
     #make sure build directory exists
-    container.exec_run(cmd="mkdir -p build")
+    container.exec_run(cmd="mkdir -p build", workdir=source_path)
 
-    compile_result = container.exec_run(cmd=f"cmake -S {source_path} -B build -DCMAKE_TOOLCHAIN_FILE=/home/developer/toolchain/arm-none-eabi-gcc.cmake")
+    compile_result = container.exec_run(cmd=f"cmake -S {source_path} -B build -DCMAKE_TOOLCHAIN_FILE=/home/developer/toolchain/arm-none-eabi-gcc.cmake", workdir=source_path)
     assert compile_result.exit_code == 0, f"cmake failed with output: {compile_result.output.decode('utf-8')}"
     print("cmake succeeded")
-    make_result = container.exec_run(cmd="make -C build")
+    make_result = container.exec_run(cmd="make -C build", workdir=source_path)
     assert make_result.exit_code == 0, f"make failed with output: {make_result.output.decode('utf-8')}"
     print("make succeeded")
     print("Test passed")
